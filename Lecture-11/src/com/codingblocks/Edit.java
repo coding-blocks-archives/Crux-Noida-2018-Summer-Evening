bf2b72bc0e256de2ddcfd4c51954a2f4ade9dabd
KeenWarrior <anujgargcse@gmail.com>
2018-06-30 17:41:56 +0530

    lec 11

--- Lecture-11/src/com/codingblocks/Edit.java
+++ Lecture-11/src/com/codingblocks/Edit.java
@@ -6,7 +6,18 @@ public class Edit {
         String first = "mohit";
         String second = "mohini";
 
-        System.out.println(edit(first, second, first.length(), second.length()));
+
+        int[][] mem = new int[first.length()+1][second.length()+1];
+
+        for (int i = 0; i <= first.length(); i++) {
+            for (int j = 0; j <= second.length() ; j++) {
+                mem[i][j] = -1;
+            }
+        }
+
+        System.out.println(edit(first, second, first.length(), second.length(), mem));
+
+
     }
 
     public static int edit(String first, String second, int f, int s){
@@ -25,8 +36,31 @@ public class Edit {
             int replace = 1 + edit(first, second, f-1, s-1);
 
             return Math.min(insert, Math.min(delete, replace));
+        }
+    }
 
+    public static int edit(String first, String second, int f, int s, int[][] mem){
+
+        if (f == 0){
+            return s;
+        } else if (s == 0){
+            return f;
         }
 
+        if (mem[f][s] != -1){
+            return mem[f][s];
+        }
+
+        if (first.charAt(f-1) == second.charAt(s-1)){
+            mem[f][s] = edit(first, second, f-1, s-1, mem);
+        } else {
+            int insert = 1 + edit(first, second, f, s-1, mem);
+            int delete = 1 + edit(first, second, f-1, s, mem);
+            int replace = 1 + edit(first, second, f-1, s-1, mem);
+
+            mem[f][s] = Math.min(insert, Math.min(delete, replace));
+        }
+
+        return mem[f][s];
     }
 }
