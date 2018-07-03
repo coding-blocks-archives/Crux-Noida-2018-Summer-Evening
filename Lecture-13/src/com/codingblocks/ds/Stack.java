9ea833d39c5bf6d3293ca7ebd0bbcc5486ca6ea3
KeenWarrior <anujgargcse@gmail.com>
2018-07-03 17:58:22 +0530

    lect 13

--- Lecture-13/src/com/codingblocks/ds/Stack.java
+++ Lecture-13/src/com/codingblocks/ds/Stack.java
@@ -32,4 +32,31 @@ public class Stack {
         return this.data[this.top--];
     }
 
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+
+        try {
+
+            stack.push(234);
+            stack.push(546);
+            stack.push(76);
+            stack.push(26534);
+            stack.push(65);
+
+            System.out.println(stack.pop());
+            System.out.println(stack.pop());
+            System.out.println(stack.pop());
+            System.out.println(stack.pop());
+            System.out.println(stack.pop());
+
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+        } finally {
+            System.out.println("This will happen anyway");
+        }
+
+        System.out.println("This still works");
+
+    }
+
 }
