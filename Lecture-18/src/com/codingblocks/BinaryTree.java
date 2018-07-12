91c97ae7cd7f1ef57bdbc99af10dc4feb1607c96
KeenWarrior <anujgargcse@gmail.com>
2018-07-12 15:41:20 +0530

    efgwe

--- Lecture-18/src/com/codingblocks/BinaryTree.java
+++ Lecture-18/src/com/codingblocks/BinaryTree.java
@@ -8,7 +8,7 @@ public class BinaryTree {
     private Node root;
 
     public void populate(){
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner("12 true 6 true 23 false false true 40 true 21 false false false true 13 false false");
         System.out.println("Value of root node ");
         int value = scanner.nextInt();
 
@@ -41,14 +41,93 @@ public class BinaryTree {
         }
     }
 
-    private class Node {
+    public void display(){
+        display(root, "");
+    }
+
+    private void display(Node node, String indent) {
+        if (node == null){
+            return;
+        }
+
+        System.out.println(indent + node.value);
+
+        display(node.left, indent + "\t");
+        display(node.right, indent + "\t");
+    }
+
+    public boolean exists(int value){
+        return exists(root, value);
+    }
+
+    private boolean exists(Node node, int value) {
+        if (node == null){
+            return false;
+        }
+
+        if (node.value == value){
+            return true;
+        }
+
+        return exists(node.left, value) || exists(node.right, value);
+    }
+
+    public Node find(int value){
+        return find(root, value);
+    }
+
+    private Node find(Node node, int value) {
+        if (node == null){
+            return null;
+        }
+
+        if (node.value == value){
+            return node;
+        }
+
+        Node left = find(node.left, value);
+        Node right = find(node.right, value);
+
+        if (left != null){
+            return left;
+        }
+
+        if (right != null){
+            return right;
+        }
+
+        return null;
+
+    }
+
+    public int diameter(){
+        return height(root.left) + height(root.right) + 1;
+    }
+
+    public int height(Node node){
+        if (node == null){
+            return 0;
+        }
+
+        int left = height(node.left);
+        int right = height(node.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+
+    public class Node {
 
         private int value;
         private Node left;
         private Node right;
 
-        public Node(int value) {
+        private Node(int value) {
             this.value = value;
+        }
+
+        public int getValue() {
+            return value;
         }
     }
 }
--- Lecture-18/src/com/codingblocks/Main.java
+++ Lecture-18/src/com/codingblocks/Main.java
@@ -6,5 +6,15 @@ public class Main {
 	// write your code here
         BinaryTree tree = new BinaryTree();
         tree.populate();
+
+//        tree.display();
+
+        System.out.println(tree.diameter());
+
+        BinaryTree.Node node = tree.find(40);
+
+        System.out.println(tree.height(node));
+
+        System.out.println(node.getValue());
     }
 }
