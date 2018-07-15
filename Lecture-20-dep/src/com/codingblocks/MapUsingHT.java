e3f90c8220b36baeb20982463034a88e997a3991
KeenWarrior <anujgargcse@gmail.com>
2018-07-15 17:04:23 +0530

    Lect 20

--- Lecture-20-dep/src/com/codingblocks/MapUsingHT.java
+++ Lecture-20-dep/src/com/codingblocks/MapUsingHT.java
@@ -7,10 +7,16 @@ public class MapUsingHT<K, V> extends Object{
 
     private ArrayList<LinkedList<Node>> list = new ArrayList<>();
 
+    private int size;
+
+    private float loadFactor = 2;
+
     public MapUsingHT(){
         for (int i = 0; i < 100; i++) {
             list.add(new LinkedList<>());
         }
+
+        size = 0;
     }
 
     public V get(K key){
@@ -36,6 +42,41 @@ public class MapUsingHT<K, V> extends Object{
         }
 
         list.get(location).add(new Node(key, value));
+        size++;
+
+        float load = size / (float) list.size();
+
+        if (load > loadFactor){
+            rehash();
+        }
+    }
+
+    private void rehash() {
+        ArrayList<LinkedList<Node>> prev = list;
+
+        this.list = new ArrayList<>();
+
+        for (int i = 0; i < prev.size() * 2; i++) {
+            list.add(new LinkedList<>());
+        }
+
+        size = 0;
+
+        for (LinkedList<Node> nodes : prev ) {
+            for (Node node : nodes){
+                put(node.key, node.value);
+            }
+        }
+
+//        for (int i = 0; i < prev.size(); i++) {
+//            LinkedList<Node> nodes = prev.get(i);
+//
+//            for (int j = 0; j < nodes.size(); j++) {
+//                Node node = nodes.get(j);
+//
+//                put(node.key, node.value);
+//            }
+//        }
     }
 
     public void remove(K key){
@@ -44,6 +85,7 @@ public class MapUsingHT<K, V> extends Object{
         for (Node node : list.get(location)){
             if (node.key.equals(key)){
                 list.get(location).remove(node);
+                size--;
                 return;
             }
         }
@@ -59,6 +101,10 @@ public class MapUsingHT<K, V> extends Object{
         }
 
         return false;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
     }
 
     private class Node {
