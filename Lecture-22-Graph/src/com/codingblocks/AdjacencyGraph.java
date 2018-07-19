14032f70e384408a478c15390c13000971cba5c0
KeenWarrior <anujgargcse@gmail.com>
2018-07-19 17:20:26 +0530

    Lecture 22 graph

--- Lecture-22-Graph/src/com/codingblocks/AdjacencyGraph.java
+++ Lecture-22-Graph/src/com/codingblocks/AdjacencyGraph.java
@@ -79,6 +79,110 @@ public class AdjacencyGraph<T> {
 
     }
 
+
+    public void BFT(){
+        if (vertices.isEmpty()){
+            return;
+        }
+
+        Set<Vertex> set = new HashSet<>();
+        Queue<Vertex> queue = new LinkedList<>();
+
+        Vertex v = vertices.get(0);
+
+        set.add(v);
+        queue.add(v);
+
+        while (!queue.isEmpty()){
+            Vertex vertex = queue.remove();
+
+            System.out.println(vertex.value);
+
+            for (Vertex padosi : vertex.nbrs){
+                if (!set.contains(padosi)){
+                    set.add(padosi);
+                    queue.add(padosi);
+                }
+            }
+        }
+
+    }
+
+
+    public void connectedComponent(){
+        if (vertices.isEmpty()){
+            return;
+        }
+
+        Set<Vertex> set = new HashSet<>();
+        Queue<Vertex> queue = new LinkedList<>();
+
+        for (Vertex v: vertices) {
+
+            if (set.contains(v)){
+                continue;
+            }
+
+            set.add(v);
+            queue.add(v);
+
+            while (!queue.isEmpty()){
+                Vertex vertex = queue.remove();
+
+                System.out.print(vertex.value + " ");
+
+                for (Vertex padosi : vertex.nbrs){
+                    if (!set.contains(padosi)){
+                        set.add(padosi);
+                        queue.add(padosi);
+                    }
+                }
+            }
+
+            System.out.println();
+        }
+
+
+
+    }
+
+    public void shortestPath(){
+        if (vertices.isEmpty()){
+            return;
+        }
+
+        Set<Vertex> set = new HashSet<>();
+        Queue<Vertex> queue = new LinkedList<>();
+
+        Vertex v = vertices.get(0);
+
+        int dist = 0;
+
+        set.add(v);
+        queue.add(v);
+        queue.add(null);
+
+        while (!queue.isEmpty()){
+            Vertex vertex = queue.remove();
+
+            if (vertex == null){
+                dist++;
+                queue.add(null);
+                continue;
+            }
+
+            System.out.println(vertex.value + " : " + dist);
+
+            for (Vertex padosi : vertex.nbrs){
+                if (!set.contains(padosi)){
+                    set.add(padosi);
+                    queue.add(padosi);
+                }
+            }
+        }
+
+    }
+
     private class Vertex {
         private T value;
         private LinkedList<Vertex> nbrs;
--- Lecture-22-Graph/src/com/codingblocks/Main.java
+++ Lecture-22-Graph/src/com/codingblocks/Main.java
@@ -14,18 +14,22 @@ public class Main {
         graph.addVertex("Gangadhar");
         graph.addVertex("Shaktiman");
 
-        graph.addEdge("Mona", "Shobhit");
+//        graph.addEdge("Mona", "Shobhit");
         graph.addEdge("Shobhit", "Ranu");
         graph.addEdge("Ranu", "Gangadhar");
         graph.addEdge("Mona", "Mohan");
         graph.addEdge("Mohan", "Shaktiman");
-        graph.addEdge("Mohan", "Geeta");
+//        graph.addEdge("Mohan", "Geeta");
         graph.addEdge("Shobhit", "Geeta");
         graph.addEdge("Ranu", "Geeta");
 
 
-        System.out.println(graph.BFS("Geeta"));
-        System.out.println(graph.BFS("Geet"));
+//        System.out.println(graph.BFS("Geeta"));
+//        System.out.println(graph.BFS("Geet"));
 //        graph.showRelations();
+
+//        graph.shortestPath();
+
+        graph.connectedComponent();
     }
 }
